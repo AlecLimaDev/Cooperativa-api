1c1c8e7875f9d47d8910913cdaaa14539305ef1b
Alec Lima <aleclimadev@gmail.com>
2024-03-09 22:58:40 -0300

    perf: archived deleted

--- prisma/migrations/20240121133024_create_cooperativa/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "cooperativas" (
-    "id" SERIAL NOT NULL,
-    "descricao" TEXT NOT NULL,
-    "codigos" TEXT NOT NULL,
-
-    CONSTRAINT "cooperativas_pkey" PRIMARY KEY ("id")
-);
